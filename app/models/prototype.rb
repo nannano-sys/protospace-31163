b2899fc1db67b686a7851722ce8cb0ae2125da12
nannano-sys <nannano1868@gmeil.com>
2020-10-29 12:02:59 +0900

    first commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,15 @@
+class Prototype < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_one_attached :image
+
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+  
+end
